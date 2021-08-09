0d04a5f9a697005ed2ebcf1735514c7f10c24497
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-09 20:13:16 +0100

    master

--- index.js
+++ index.js
@@ -22,8 +22,30 @@
     4. Log to the console to demonstrate it works
 */
 
+/*
+    Task: Get all the name elements from the DOM
+    1. Create a new 'let' called 'allNamesDOMCollection'
+    2. Store all the elements of classname 'name' in 'allNamesDOMCollection'
+    3. Log to the console 'allNamesDOMCollection' to demonstrate that an HTMLCollection exists
+    4. Log out the first index of allNamesDOMCollection and add 'textContent' to view a name
+*/
 
- document.getElementById('searchInput').addEventListener('keyup', function(event){
-    let searchQuery = event.target.value.toLowerCase()
+/*
+    Task: Loop over all our names
+    1. Create a new for loop with a incremental counter, called 'counter'
+    2. Make sure the counter is less than the allNamesDOMCollection.length in the loop conditional
+    3. Declare a const called 'currentName' and store the textContext
+    4. Make sure the 'currentName' is lowercase
+    5. Log out the 'currentName' so we can see all the names from the loop!
+*/
+
+ document.getElementById("searchInput").addEventListener('keyup', function(event){
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName("name")
+    for (let i = 0; i < allNamesDOMCollection.length ; i++){
+        const currentName = allNamesDOMCollection[i].textContent.toLowerCase()
+        console.log(currentName)
+    };
+
 })
 
