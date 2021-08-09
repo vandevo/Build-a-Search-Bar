aac111061c81f6b3f6919a5d7f0c31a565a1a9ad
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-09 20:14:12 +0100

    commit

--- index.js
+++ index.js
@@ -42,8 +42,8 @@
  document.getElementById("searchInput").addEventListener('keyup', function(event){
     let searchQuery = event.target.value.toLowerCase();
     let allNamesDOMCollection = document.getElementsByClassName("name")
-    for (let i = 0; i < allNamesDOMCollection.length ; i++){
-        const currentName = allNamesDOMCollection[i].textContent.toLowerCase()
+    for (let counter = 0; counter < allNamesDOMCollection.length ; counter++){
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase()
         console.log(currentName)
     };
 
